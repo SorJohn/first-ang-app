219105e7e761040c9604bd9b6345c9d587c1d297
João Silva <joaodavid.gregorio@gmail.com>
2021-03-17 15:51:45 +0000

    Commit

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -16,6 +16,7 @@ export class ListComponent implements OnInit {
     this._http.getBeer().subscribe(data => {
       this.brews = data;
       console.log(this.brews);
+      console.log("Wow");
     });
   }
 }
